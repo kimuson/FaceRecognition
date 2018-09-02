edcf7171d8a59cb5ee97912a03b7d26cbe3a2cce
kimurayuta <gygyhumhum1101@gmail.com>
2018-09-02 13:34:55 +0900

    Initial Commit

---
+++ FaceRecognition/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FaceRecognition
+//
+//  Created by 木村優太 on 2018/09/02.
+//  Copyright © 2018年 木村優太. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
